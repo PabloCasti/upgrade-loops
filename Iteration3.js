569226915067210650daea88b239c437a6c51cc0
PabloCas <MadridCastilla@hotmail.com>
2021-11-16 08:17:44 +0100

    Iteration#3

---
+++ Iteration3.js
@@ -0,0 +1,14 @@
+// 3. Probando For...of
+
+const placesToTravel = [
+  "Japon",
+  "Venecia",
+  "Murcia",
+  "Santander",
+  "Filipinas",
+  "Madagascar",
+];
+
+for (item of placesToTravel) {
+  console.log(item);
+}
